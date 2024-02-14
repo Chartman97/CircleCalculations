1f8841dce00d499f9bd0d4c8672cef1d36745f91
Chartman97 <calebhartman97@gmail.com>
2024-02-13 19:42:27 -0700

    yes sir

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,44 @@
 import java.util.Scanner;
+
 // Take Radius(Inches)
 // Output Diameter & Area & Circumference(Inches)
 public class Main {
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        double radius, pie, diameter, area, radiusSq, circumference;
-        // Radius input & Confirmation
-        System.out.println("\nHello! If you input a Radius in Inches.\nI will give you the diameter, area, and circumference of that circle in Inches.");
-        System.out.print("Enter Radius in Inches Here ->");
-        radius = in.nextDouble();
-        // Conversions
+        double radius, area, radiusSq, circumference, diameter, pie;
         pie = Math.PI;
-        radiusSq = radius * radius;
-        diameter = 2 * radius;
-        area = pie * radiusSq;
-        circumference = 2 * pie * radius;
+
+        // Intro & Radius Input
+        System.out.println("Hello!\nMy name is \"CircleCalculation\"...\nMy only function in life is to convert... stuff");
+        System.out.println("Enter Radius, in Inches, Here ->");
+        radius = in.nextDouble();
+
+        // Conversions
+        radiusSq = radiusSq(radius);
+        diameter = diameterConvert(radius);
+        area = areaConvert(radiusSq, pie);
+        circumference = circumferenceConvert(radius, pie);
+
         // Output
-        System.out.printf("\nDiameter = %.2f Inches\nArea =  %.4f Inches\nCircumference = %.4f Inches\nGood Bye\n", diameter, area, circumference);
+        System.out.printf("\nAll answers are in Inches\nDiameter = %.2f\nArea = %.2f\nCircumference = %.2f\n", diameter, area, circumference);
+        System.out.println("Please give me 10/10 :)");
+    }
+
+    // Conversions
+    public static Double radiusSq(double radius) {
+        return radius * radius;
+    }
+
+    public static Double diameterConvert(double radius) {
+        return 2 * radius;
+    }
+
+    public static Double areaConvert(double radiusSq, double pie) {
+        return pie * radiusSq;
+    }
+
+    public static Double circumferenceConvert(double radius, double pie) {
+        return 2 * pie * radius;
     }
 }
